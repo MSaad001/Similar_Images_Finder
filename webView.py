6467461c839c3d8e01d2737169c1f5fb722a21b5
Muhammad Saad <sm7226336@gmail.com>
2023-12-16 20:32:52 +0500

    show similarity metrics
    The percentage of how much each resulting image is similar to the input image is displayed below every resulting image.

--- webView.py
+++ webView.py
@@ -22,7 +22,6 @@ model = Sequential([
     GlobalMaxPooling2D()
 ])
 
-
 st.title('Similar Images Finder')
 
 
@@ -50,13 +49,12 @@ def recommend(features1, feature_list1):
     neighbors = NearestNeighbors(n_neighbors=6, algorithm='brute', metric='euclidean')
     neighbors.fit(feature_list1)
 
-    distances, indices1 = neighbors.kneighbors([features1])
+    distances1, indices1 = neighbors.kneighbors([features1])
 
-    return indices1
+    return distances1, indices1
 
 
-# steps
-# file upload => save
+# file upload
 uploaded_file = st.file_uploader("Choose an Image")
 features = None
 print(uploaded_file)
@@ -73,7 +71,6 @@ if uploaded_file is not None:
             features = extract_features(os.path.join("uploads", uploaded_file.name), model)
 
         if features is not None:
-
             # check if features are too dissimilar
             threshold = 0.8
             dissimilarity = norm(feature_list - features, axis=1)
@@ -82,22 +79,24 @@ if uploaded_file is not None:
                 print("No results found for the input image")
             else:
                 # recommendation
-                indices = recommend(features, feature_list)
-
+                distances, indices = recommend(features, feature_list)
                 # show
                 col1, col2, col3, col4, col5, col6 = st.columns(6)
-
                 with col1:
                     st.image(filenames[indices[0][0]])
+                    st.write(f"Similarity: {distances[0][0] * 100:.2f}%")
                 with col2:
                     st.image(filenames[indices[0][1]])
+                    st.write(f"Similarity: {distances[0][1] * 100:.2f}%")
                 with col3:
                     st.image(filenames[indices[0][2]])
+                    st.write(f"Similarity: {distances[0][2] * 100:.2f}%")
                 with col4:
                     st.image(filenames[indices[0][3]])
+                    st.write(f"Similarity: {distances[0][3] * 100:.2f}%")
                 with col5:
                     st.image(filenames[indices[0][4]])
+                    st.write(f"Similarity: {distances[0][4] * 100:.2f}%")
                 with col6:
                     st.image(filenames[indices[0][5]])
-    else:
-        st.header("Upload Failed")
+                    st.write(f"Similarity: {distances[0][5] * 100:.2f}%")
